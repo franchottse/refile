1e7bebaebc4255e4144f925008e08190bfb46be1
Franchot <tsezg523@gmail.com>
2020-07-15 20:52:46 +0800

    change the layout of the GUI

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog, Text, messagebox
+from tkinter import filedialog, Text, messagebox, ttk, Menu
 import os
 import func
 
@@ -18,30 +18,100 @@ class ReFile(tk.Frame):
     def test_func(self):
         func.test_function()
 
-    # TODO: Fix the background colour resizing problem
+    # TODO: Beautify the layout of the GUI
     def configureGUI(self):
         # App title
         self.master.winfo_toplevel().title('ReFile')
+        self.master.minsize(640, 480)
+
+        # Menu
+        self.menu = Menu(self.master)
+        self.master.config(menu=self.menu)
+
+        self.subMenu = Menu(self.menu, tearoff=False)
+        self.menu.add_cascade(label='File', menu=self.subMenu)
+        self.subMenu.add_command(label='Open File', command=self.addFile)
+        self.subMenu.add_separator()
+        self.subMenu.add_command(label='Exit', command=self.master.destroy)
 
         # Create a canvas
         self.canvas = tk.Canvas(self.master, height=700,
-                                width=1200, bg='#87CEEB')
-        self.canvas.grid(sticky='NSWE')
-        self.canvas.place()
+                                width=1200, bg='#ACBFC9')
+
+        # Create a frame for displaying data
+        self.dataFrame = tk.Frame(self.master)
+        self.dataFrame.place(relwidth=0.6, relheight=0.6, relx=0.3, rely=0.1)
+        self.dataLabel = tk.Label(self.dataFrame, text='Data', bg='#18EBA0')
+
+        # Test tree view
+        cols = ('Position', 'Name', 'Score',
+                'Random Stuff', 'Another Random Stuff')
+        self.listBox = ttk.Treeview(
+            self.dataFrame, columns=cols, show='headings', selectmode=tk.BROWSE)
+        # set column headings
+        for col in cols:
+            self.listBox.column(col)
+            self.listBox.heading(col, text=col)
+
+        self.verticalScrollBar = ttk.Scrollbar(
+            self.dataFrame, orient=tk.VERTICAL, command=self.listBox.yview)
+
+        self.HorizontalScrollBar = ttk.Scrollbar(
+            self.dataFrame, orient=tk.HORIZONTAL, command=self.listBox.xview)
+
+        self.sizegrip = ttk.Sizegrip(self.dataFrame)
+
+        self.listBox.configure(xscrollcommand=self.HorizontalScrollBar.set,
+                               yscrollcommand=self.verticalScrollBar.set)
+
+        # End of test tree view
 
         # Create a frame for displaying files
-        self.fileFrame = tk.Frame(self.master, bg='white')
-        self.fileFrame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-        self.fileLabel = tk.Label(self.fileFrame, text='Files', bg='#EBA687')
-        self.fileLabel.grid(column=1, row=0, sticky='NSWE')
+        self.fileFrame = tk.Frame(self.master, bg='#20313A')
+        self.fileFrame.place(relwidth=0.8, relheight=0.2, relx=0.1, rely=0.7)
+        self.fileLabel = tk.Label(self.fileFrame, text='Files', bg='#18EBA0')
+        # self.fileLabel.grid(column=1, row=0, sticky='NSWE')
+
+        # Create a frame for displaying check boxes
+        self.checkBoxFrame = tk.Frame(self.master, bg='#172A33')
+        self.checkBoxFrame.place(
+            relwidth=0.2, relheight=0.6, relx=0.1, rely=0.1)
+        self.checkBoxLabel = tk.Label(
+            self.checkBoxFrame, text='Columns', bg='#18EBA0')
+
+        # Pack everything
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+        self.dataLabel.pack(padx=0.5, pady=0.1, fill=tk.X)
+        self.HorizontalScrollBar.pack(side=tk.BOTTOM, fill=tk.X, expand=False)
+        self.verticalScrollBar.pack(side=tk.RIGHT, fill=tk.Y, expand=False)
+        self.sizegrip.pack(in_=self.HorizontalScrollBar,
+                           side=tk.BOTTOM, anchor=tk.S+tk.E)
+        self.listBox.pack(fill=tk.BOTH, expand=True)
         self.fileLabel.pack(padx=0.5, pady=0.1)
+        self.checkBoxLabel.pack(padx=0.1, pady=0.1)
 
     def createWidgets(self):
         # Create an select files button
-        self.openFile = tk.Button(self.master, text='Select File', padx=10,
-                                  pady=5, fg='white', bg='#263D42', bd=2, command=self.addFile)
-        self.openFile.grid()
+        buttonStyle = {'padx': '10', 'pady': '5', 'fg': '#18EBA0', 'bg': '#20313A',
+                       'activebackground': '#172A33', 'activeforeground': '#18EBA0', 'bd': '2'}
+        self.openFile = tk.Button(
+            self.master, text='Select File', **buttonStyle, command=self.addFile)
         self.openFile.place(relx=0.5, rely=0.95, anchor=tk.CENTER)
+
+        # Test button
+        tk.Button(self.master, text="Show scores", **buttonStyle,
+                  command=self.show).place(relx=0.6, rely=0.95, anchor=tk.CENTER)
+
+    # Demo function
+    def show(self):
+        tempList = [['Jim', '0.33', 'What', 'Hello'], ['Dave', '0.67', 'is', ''],
+                    ['James', '0.67', 'Tkinter', 'World'], ['Eden', '0.5', '?', '']]
+        # tempList.sort(key=lambda e: e[1], reverse=True)
+
+        for i, (name, score, stuff, stuff1) in enumerate(tempList, start=1):
+            self.listBox.insert("", "end", values=(
+                i, name, score, stuff, stuff1))
+    # End of demo function
 
     def isFile(self):
         if os.path.isfile('xlList.txt'):
